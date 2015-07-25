7b518a04200a9bdf167953c588d5791583e88861
Neil Northrop <nnorthrop@gmail.com>
2015-07-24 18:38:36 -0700

    clean up files

--- lib/app_server/app/tic_tac_toe/lib/controllers/GameController.rb
+++ lib/app_server/app/tic_tac_toe/lib/controllers/GameController.rb
@@ -1,10 +1,9 @@
 require 'haml'
-
 require_relative 'ApplicationController'
 
 class GameController < ApplicationController
 	def run(board)
-		body = Haml::Engine.new(File.read('./views/board.haml')).render(Object.new, :board => board)
+		body = Haml::Engine.new(File.read('../views/board.haml')).render(Object.new, :board => board)
 		return body
 	end
 end
