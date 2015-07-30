56a377d2048b456103567cad7f2b8d83f2521a8a
Neil Northrop <nnorthrop@gmail.com>
2015-07-30 14:31:06 -0700

    add ClassLoader#run to streamline the process

--- lib/app_server/class_loader.rb
+++ lib/app_server/class_loader.rb
@@ -7,6 +7,36 @@ class ClassLoader
     @classes       = []
   end
 
+  def run
+    collect_file_names
+    file_require
+    collect_class_names
+    return classify
+  end
+
+  def file_require
+    loaded_files.each do |file|
+      requiring(file)
+    end
+  end
+
+  def requiring(file)
+    require Dir.pwd + path_to_files + file
+  end
+
+  def file_name_to_class_name(file_name)
+    file_name.chomp('.rb').split('_').each(&:capitalize!).join
+  end
+  alias to_class file_name_to_class_name
+
+  def collect_file_names
+    dir_files.each do |file|
+      next if file == '.' or file == '..'
+      loaded_files << file
+    end
+  end
+
+  private
   def directory_listing
     Dir.new(Dir.pwd + path_to_files)
   end
@@ -15,35 +45,18 @@ class ClassLoader
     directory_listing.entries
   end
 
-  def requiring(controller)
-    require Dir.pwd + path_to_files + controller
-  end
-
   def collect_class_names
-    classes << file_name_to_class_name(controller)
-  end
-
-  def collect_file_names
-    dir_files.each do |controller|
-      next if controller == "." or controller == ".."
-      loaded_files << controller
+    loaded_files.each do |file|
+      classes << to_class(file)
     end
   end
 
-  def file_name_to_class_name(file_name)
-    file_name.chomp('.rb').split('_').each(&:capitalize!).join
-  end
-
-  def run
-    collect_file_names
-    return classify
-  end
-
-  private
   def classify
+    klasses = []
     classes.each do |klass|
-      Object.const_get(klass).new
+      klasses << Object.const_get(klass).new
     end
+    return klasses
   end
 
 end
