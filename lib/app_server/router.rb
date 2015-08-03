08fb53e28acb46a31f9f3135a56bb45b181bfd3e
Neil Northrop <nnorthrop@gmail.com>
2015-08-03 08:49:43 -0700

    add route loading from json file

--- lib/app_server/router.rb
+++ lib/app_server/router.rb
@@ -1,8 +1,16 @@
+require 'json'
+
 class Router
-  attr_accessor :routes_path, :loader
+  attr_accessor :routes_path, :loader, :routes
 
   def initialize(opts={})
     @routes_path = opts[:routes]
     @loader      = opts[:loader]
+    @routes      = load_routes
   end
+
+  def load_routes
+    JSON.parse(File.read(routes_path), symbolize_names: true)
+  end
+
 end
