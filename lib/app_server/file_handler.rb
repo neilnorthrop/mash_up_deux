197d22b3629d3fb066ada93250b05affea0d29ea
Neil Northrop <nnorthrop@gmail.com>
2015-08-06 13:04:33 -0700

    refactor FileHandler#build_body

--- lib/app_server/file_handler.rb
+++ lib/app_server/file_handler.rb
@@ -1,3 +1,7 @@
+require 'haml'
+require 'haml_builder'
+require 'html_builder'
+
 class FileHandler
 
   attr_accessor :path, :body, :response_code, :content_type, :file_size
@@ -30,22 +34,50 @@ class FileHandler
 
   def initialize(path="")
     @path          = clean_path(path)
+    @builder       = pick_builder(path)
     @body          = ""
     @response_code = ""
     @content_type  = ""
     @file_size     = 0
   end
 
+  def pick_builder(path)
+    return eval("#{extention_type(path).capitalize}Builder.new")
+  end
+
   def build_body(path)
-    File.open(path, "rb") do |file|
-      @body = file
-      @file_size = file.size
+    case extention_type(path)
+    when 'html'
+      file = build_html(path)
+      get_size(file)
+      return file
+    when 'haml'
+      file = build_haml(path)
+      get_size(file)
+      return file
+    else
+      file = build_static(path)
+      get_size(file)
+      return file
     end
   end
 
+  def build_html(path)
+    @body = File.read(path)
+  end
+
+  def build_haml(path)
+    p path
+    @body = Haml::Engine.new(File.read(path)).render
+  end
+
   def get_content_type(path)
-    ext = File.extname(path).split('.').last
+    ext = extention_type(path)
     CONTENT_TYPE_MAPPING.fetch(ext, DEFAULT_CONTENT_TYPE)
+  end
+
+  def extention_type(path)
+    File.extname(path).split('.').last
   end
 
   def handle_file
@@ -53,10 +85,15 @@ class FileHandler
       build_body(@path)
       @response_code = RESPONSE_CODE.rassoc('OK').join("/")
       @content_type = get_content_type(@path)
+      p self
     else
       build_body('./public/404.html')
       @response_code = RESPONSE_CODE.rassoc('Not Found').join("/")
     end
+  end
+
+  def get_size(file)
+    self.file_size = file.size
   end
 
   def clean_path(path)
