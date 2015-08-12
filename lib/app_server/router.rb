fcc592fac6fbf787ce007f8f47c18e8f810203ba
Neil Northrop <nnorthrop@gmail.com>
2015-08-12 10:29:34 -0700

    add ClassLoader and a refactor of Router#route

--- lib/app_server/router.rb
+++ lib/app_server/router.rb
@@ -1,16 +1,29 @@
 require 'json'
+require_relative 'file_handler'
+require_relative 'class_loader'
 
 class Router
   attr_accessor :routes_path, :loader, :routes
 
   def initialize(opts={})
-    @routes_path = opts[:routes]
-    @loader      = opts[:loader]
-    @routes      = load_routes
+    @routes_path = './lib/app_server/config/routes.json'
+    @routes      = load_class_loader
+  end
+
+  def load_class_loader
+    class_loader = ClassLoader.new('/lib/app_server/app/tic_tac_toe/lib/controllers/')
+    class_loader.run
+    return class_loader
   end
 
   def load_routes
     JSON.parse(File.read(routes_path), symbolize_names: true)
+  end
+
+  def route(request)
+    file = FileHandler.new(request[:resource])
+    file.handle_file
+    return Response.build_header(file)
   end
 
 end
