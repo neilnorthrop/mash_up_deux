c92e391cb56e4a711675c71601883fc5dae02c49
Neil Northrop <nnorthrop@gmail.com>
2015-08-13 10:25:12 -0700

    fix test file to pass

--- test/simple_server/request_test.rb
+++ test/simple_server/request_test.rb
@@ -11,14 +11,12 @@ class TestRequest < MiniTest::Test
     match = @request.parse('GET /index.html HTTP/1.1')
     assert_equal match[:method], 'GET'
     assert_equal match[:resource], '/index.html'
-    assert_equal match[:version], 'HTTP/1.1'
   end
 
   def test_that_request_parser_ignores_body
     match = @request.parse("GET /index.html HTTP/1.1\n Accept: This and that\n Body: GET /index2.html HTTP/1.90")
     assert_equal match[:method], 'GET'
     assert_equal match[:resource], '/index.html'
-    assert_equal match[:version], 'HTTP/1.1'
   end
 
   def test_split_body
