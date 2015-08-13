9c38ac71149ca4bea5e1ff0d270ccfc608e0cc1c
Neil Northrop <nnorthrop@gmail.com>
2015-08-13 10:24:55 -0700

    fix test file to pass

--- test/simple_server/file_handler_test.rb
+++ test/simple_server/file_handler_test.rb
@@ -4,7 +4,7 @@ require 'minitest/autorun'
 class FileHandlerTest < MiniTest::Test
 
   def setup
-    @file_handler = FileHandler.new
+    @file_handler = FileHandler.new("", "")
   end
 
   def test_get_content_type
@@ -13,25 +13,29 @@ class FileHandlerTest < MiniTest::Test
   end
 
   def test_handle_file_found
-    @file_handler.handle_file('./test/simple_server/test.html')
-    assert_equal '200/OK', @file_handler.response_code
+    file_handle = FileHandler.new("index.html", "")
+    file_handle.path = './test/simple_server/test.html'
+    file_handle.handle_file
+    assert_equal '200/OK', file_handle.response_code
   end
 
   def test_handle_file_not_found
-    @file_handler.handle_file('./test/hello.index')
-    assert_equal '404/Not Found', @file_handler.response_code
+    file_handle = FileHandler.new("index.html", "")
+    file_handle.path = './test/hello.html'
+    file_handle.handle_file
+    assert_equal '404/Not Found', file_handle.response_code
   end
 
   def test_requested_file_joins_a_resource
-    assert_equal './lib/app_server/app/tic_tac_toe/views/index.html', @file_handler.clean_path('/index.html')
+    assert_equal './lib/app_server/app/tic_tac_toe/public/index.html', @file_handler.clean_path('/index.html')
   end
 
   def test_requested_file_keeps_directories
-    assert_equal './lib/app_server/app/tic_tac_toe/views/hello/index.html', @file_handler.clean_path('/../../../hello/index.html')
+    assert_equal './lib/app_server/app/tic_tac_toe/public/hello/index.html', @file_handler.clean_path('/../../../hello/index.html')
   end
 
   def test_requested_file_removes_change_directories
-    assert_equal './lib/app_server/app/tic_tac_toe/views/index.html', @file_handler.clean_path('/../../hello/../index.html')
+    assert_equal './lib/app_server/app/tic_tac_toe/public/index.html', @file_handler.clean_path('/../../hello/../index.html')
   end
 
 end
